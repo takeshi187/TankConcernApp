1988bb44bab97299c02202956017dd10b5ed3a7b
Richard <94477473+kori43@users.noreply.github.com>
2025-03-19 23:08:57 +0500

    Добавьте файлы проекта.

---
+++ TankConcernApp/LogInWin.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TankConcernApp
+{
+    public partial class LogInWin : Window
+    {
+        public LogInWin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Entry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_Clean_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBox_Login.Clear();
+            PassBox_Password.Clear();
+        }
+    }
+}
